6595d70d537c22b31ad233779d74dd92bd525b54
saquib momin <saquibmomin42@gmail.com>
2026-06-09 07:02:31 +0530

    Add files via upload

---
+++ electricity bill calculation.py
@@ -0,0 +1,8 @@
+units = float(input("Enter number of electricity units consumed: "))
+
+# Charge rate per unit is 7 rupees
+rate = 7.0
+total_bill = units * rate
+
+print("Units used:", units)
+print("Total Electricity Bill:", total_bill)

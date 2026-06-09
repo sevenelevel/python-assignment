00211cdb364f7b7b2377e6178f164a1c29f74a4e
saquib momin <saquibmomin42@gmail.com>
2026-06-09 08:11:46 +0530

    Add files via upload

---
+++ variable reference analysis.py
@@ -0,0 +1,8 @@
+# Strings vs Lists memory behavior
+str1 = "hello"
+str2 = "hello"
+print("Do strings share memory identity?:", str1 is str2)
+
+arr1 = ["hello"]
+arr2 = ["hello"]
+print("Do lists share memory identity?:", arr1 is arr2)

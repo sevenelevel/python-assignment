7cf3f32ee49111ae493bb03c134dd9c20559768b
saquib momin <saquibmomin42@gmail.com>
2026-06-09 08:02:23 +0530

    Add files via upload

---
+++ employee record reference.py
@@ -0,0 +1,9 @@
+#Employee Record Reference Verification
+
+employee1 = {"id":101, "name":"Saquib"}
+employee2 = employee1
+
+if employee1 is employee2:
+    print("Same employee record")
+else:
+    print("Different employee records")
